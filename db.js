f205214d3d6030074a9a37cfbc1f93be149dda6d
vivek Kumar <vivek746288@gmail.com>
2025-08-04 01:09:37 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const db = mysql.createPool({
   host: process.env.DB_HOST || 'mysql-vivek.alwaysdata.net',
   user: process.env.DB_USER || 'vivek_vivek',
-  password: process.env.DB_PASSWORD || 'V7462881297v',
+  password: process.env.DB_PASSWORD || 'V7462881297@v',
   database: process.env.DB_NAME || 'vivek_vivek',
   waitForConnections: true,
   connectionLimit: 10,
